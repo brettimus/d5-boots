1150539a7612863a94c74503c0678d7e19da6282
brettimus <brbeut@gmail.com>
2015-02-15 21:05:03 -0800

    first test build

--- src/js/d5.js
+++ src/js/d5.js
@@ -1,13 +1,9 @@
-var d3      = require("d3"),
-    $       = require ("jquery"),
-    stampit = require("stampit");
+var d3      = require("d3");
 
 global.d5 = (function(){
     return {
         me: function() { console.log("Hey hey hey"); console.log(this); },
-        stampit: stampit,
         d3: d3,
-        $: $,
     };
 })();
 
